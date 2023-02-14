91655e3faf13809b44a527fe5d9af615f648506e
AaronBarcos <abarcoscaballero@gmail.com>
2023-02-14 15:59:25 +0100

    it 1 and 2

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,13 @@
+const Celebrity = require("../models/Celebrity.model");
+
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+
+
+
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
 
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+// Rutas de celebrities
+const celebritiesRoutes = require("./celebrities.routes.js")
+router.use("/celebrities", celebritiesRoutes)
+// Rutas de movies
+const moviesRoutes = require("./movies.routes.js")
+router.use("/movies", moviesRoutes)
+
+router.get("/create", async (req, res, next) => {
+  const response = await Celebrity.create({
+    name: "Jose Luis",
+    age: 26
+  })
+})
 
 module.exports = router;
